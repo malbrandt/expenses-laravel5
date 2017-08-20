2043c1ff2fce92d28639ffdd60220a534ef43a0c
Marek Malbrandt <marek.malbrandt@gmail.com>
2017-08-20 13:58:57 +0200

    migrations

---
+++ database/migrations/2017_08_20_085553_create_payments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(config('database.payments'), function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('expense_id');
+            $table->float('amount');
+            $table->tinyInteger('assent')
+                ->nullable()
+                ->comment = "-1 => rejected,\n" .
+                            " 1 => accepted.";
+            $table->datetime('assent_modified_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists(config('database.payments'));
+    }
+}
